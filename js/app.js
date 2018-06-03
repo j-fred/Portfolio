53f476f76e16062438b220a265e1585934fb6a70
j-fred <jfglamport@gmail.com>
2018-06-03 19:52:22 +0400

    ajout branch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 $(function () {
-    $('body').scrollspy({ target: '#ma-navbar' });
+    $('body').scrollspy({ target: '#menu' });
+    // On recupere la position du bloc par rapport au haut du site
+    var posYmenu = $("#menu").offset().top;
+
+    //Au scroll dans la fenetre on déclenche la fonction
+    $(window).scroll(function () {
+
+        //si on a defile de plus de 150px du haut vers le bas
+        if ($(this).scrollTop() > posYmenu) {
+
+            //on ajoute la classe "fixNavigation" a <div id="navigation">
+            $('#menu').addClass("fixNavigation");
+        } else {
+
+            //sinon on retire la classe "fixNavigation" a <div id="navigation">
+            $('#menu').removeClass("fixNavigation");
+        }
+    });
 });
